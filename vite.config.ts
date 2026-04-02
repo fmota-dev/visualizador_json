006474f1e268cf4044840031e4dc069f5463c92f
fmota <filipe.motasl@outlook.com>
2026-04-02 16:36:35 -0300

    chore: prepara deploy no github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/explorador_estruturas/",
   define: {
     global: "globalThis",
   },
